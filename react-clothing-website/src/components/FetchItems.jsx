8d05200d25cf14280a177fee2a824a479c55e823
dhruval soni <dhruvalsoni717@gmail.com>
2024-04-21 16:25:51 +0530

    img

--- react-clothing-website/src/components/FetchItems.jsx
+++ react-clothing-website/src/components/FetchItems.jsx
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { MenSectionProductsAction } from "../store/MenSectionProductsSlice";
-import { fetchStatusAction } from "../store/fetchStatusSlice";
 
 const FetchItems = () => {
   const fetchStatus = useSelector((store) => store.fetchStatus);
@@ -12,13 +11,10 @@ const FetchItems = () => {
 
     const controller = new AbortController();
     const signal = controller.signal;
-    // dispatch(fetchStatusAction.markFetchingStarted());
 
     fetch("http://localhost:3001/items", { signal })
       .then((res) => res.json())
       .then(({ items }) => {
-        // dispatch(fetchStatusAction.markFetchDone());
-        // dispatch(fetchStatusAction.markFetchingFinished());
         dispatch(MenSectionProductsAction.addInitialItems(items[0]));
       });
     return () => {
